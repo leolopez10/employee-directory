96a907c5669b48841a7beb2f419254bb55c01001
lil L <57925562+leolopez10@users.noreply.github.com>
2020-02-22 14:35:52 -0600

    Table has data placed into the table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -12,100 +12,107 @@ export default function MaterialTableDemo() {
     ],
     data: [
         {
-            firstName: 'Zerya',
+            firstName: 'Lacy',
             lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
-            department: 'Accounting',
-            salary: '160,000'
+            id: '1',
+            role: 'CEO',
+            department: 'Operations',
+            salary: '250,000'
         },
         {
-            firstName: 'Zerya',
-            lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
+            firstName: 'Dan',
+            lastName: 'Jango',
+            id: '2',
+            role: 'Public Accountant',
             department: 'Accounting',
-            salary: '160,000'
+            salary: '90,000'
         },
         {
-            firstName: 'Zerya',
-            lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
-            department: 'Accounting',
-            salary: '160,000'
+            firstName: 'Josh',
+            lastName: 'Klash',
+            id: '3',
+            role: 'Software Engineer',
+            department: 'Production',
+            salary: '110,000'
         },
         {
-            firstName: 'Zerya',
-            lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
-            department: 'Accounting',
-            salary: '160,000'
+            firstName: 'Lucy',
+            lastName: 'Degaldo',
+            id: '4',
+            role: 'Software Engineer',
+            department: 'Production',
+            salary: '110,000'
         },
         {
-            firstName: 'Zerya',
-            lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
-            department: 'Accounting',
-            salary: '160,000'
+            firstName: 'Blake',
+            lastName: 'Tadder',
+            id: '5',
+            role: 'Front-End Developer',
+            department: 'Development',
+            salary: '80,000'
         },
         {
-            firstName: 'Zerya',
-            lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
-            department: 'Accounting',
-            salary: '160,000'
+            firstName: 'Kirstan',
+            lastName: 'Strave',
+            id: '6',
+            role: 'Front-End Developer',
+            department: 'Development',
+            salary: '95,000'
         },
         {
-            firstName: 'Zerya',
-            lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
-            department: 'Accounting',
-            salary: '160,000'
+            firstName: 'Kim',
+            lastName: 'Drago',
+            id: '7',
+            role: 'Back-End Developer',
+            department: 'Development',
+            salary: '100,000'
         },
         {
-            firstName: 'Zerya',
-            lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
-            department: 'Accounting',
-            salary: '160,000'
+            firstName: 'Urban',
+            lastName: 'Impatchu',
+            id: '8',
+            role: 'Back-End Developer',
+            department: 'Development',
+            salary: '100,000'
         },
         {
-            firstName: 'Zerya',
-            lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
-            department: 'Accounting',
-            salary: '160,000'
+            firstName: 'Adam',
+            lastName: 'Cliche',
+            id: '9',
+            role: 'System Architect',
+            department: 'Operations',
+            salary: '110,000'
         },
         {
-            firstName: 'Zerya',
-            lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
-            department: 'Accounting',
-            salary: '160,000'
+            firstName: 'Rene',
+            lastName: 'Grossman',
+            id: '10',
+            role: 'CIO',
+            department: 'Operations',
+            salary: '225,000'
         },
         {
-            firstName: 'Zerya',
-            lastName: 'Bravo',
-            id: '50',
-            role: 'CPA',
-            department: 'Accounting',
-            salary: '160,000'
+            firstName: 'Braxton',
+            lastName: 'Smallwood',
+            id: '11',
+            role: 'DevOps',
+            department: 'Development',
+            salary: '110,000'
         },
-        
+        {
+            firstName: 'Zada',
+            lastName: 'Habrid',
+            id: '12',
+            role: 'Distribution Supplier',
+            department: 'Marketing',
+            salary: '95,000'
+        }
     ],
   });
 
   return (
     <MaterialTable
-      title="Editable Example"
+      title=""
       columns={state.columns}
       data={state.data}
       editable={{

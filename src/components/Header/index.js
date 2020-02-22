dc9283dea805b4bfbbefb7814f8f932914a66865
lil L <57925562+leolopez10@users.noreply.github.com>
2020-02-22 13:28:28 -0600

    updated table

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -68,16 +68,8 @@ export default function SearchAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-            Material-UI
+            Employee Directory
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
